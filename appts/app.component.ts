9e1a6aa03023fd49b90e697da9e8d9916baef6ac
王齐源 <wqy813853090@live.cn>
2016-10-13 11:41:02 +0800

    init res

---
+++ appts/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { AlertComponent } from 'ng2-bootstrap/ng2-bootstrap';
+@Component({
+  selector: 'my-app',
+  templateUrl: '../views/app.html',
+
+})
+export class AppComponent {
+}
---
+++ appts/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+@NgModule({
+  imports: [BrowserModule,
+    Ng2BootstrapModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
